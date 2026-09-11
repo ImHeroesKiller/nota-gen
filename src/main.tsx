9f3841130e597056b44c6baba26f350c9bd27c28
ImHeroesKiller <148073026+ImHeroesKiller@users.noreply.github.com>
2026-09-11 23:51:49 +0700

    Load ATS layout corrections after legacy workspace CSS

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./styles/all-tools-modern.css";
 import "./styles/tool-workspace-v4.css";
+import "./styles/recruitment-pipeline.css";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
