c240d864ba02d9947595e36de3c7ac28dee63428
ImHeroesKiller <148073026+ImHeroesKiller@users.noreply.github.com>
2026-09-11 22:17:13 +0700

    Add global application error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import ToolHub from './components/ToolHub';
+import ErrorBoundary from './components/ErrorBoundary';
 
 export default function App() {
-  return <ToolHub />;
+  return (
+    <ErrorBoundary>
+      <ToolHub />
+    </ErrorBoundary>
+  );
 }
